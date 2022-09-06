06e59b06ee57dbe5314f6e53023c5bd497ba69ca
KreoX <soldimksol@gmail.com>
2022-09-06 19:30:32 +0300

    Ex67

---
+++ Ex67/Program.cs
@@ -0,0 +1,20 @@
+﻿int Prompt(string message)
+{
+    Console.Write(message);
+    int res = Convert.ToInt32(Console.ReadLine());
+    return res;
+}
+
+int NumSum(int sum, int num)
+{
+    if (num < 10) return sum + num;
+    else
+    {
+        sum+= num%10;
+        return (NumSum(sum, num/10));
+    }
+}
+
+int num = Prompt("Введите число:");
+
+Console.WriteLine(NumSum(0,num));
