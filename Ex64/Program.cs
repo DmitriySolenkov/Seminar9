69bbe903f4d61afa6e0d10699071854cd1b585a5
KreoX <soldimksol@gmail.com>
2022-09-06 19:12:37 +0300

    Ex64

---
+++ Ex64/Program.cs
@@ -0,0 +1,25 @@
+﻿int Prompt(string message)
+{
+    Console.Write(message);
+    int res = Convert.ToInt32(Console.ReadLine());
+    return res;
+}
+
+string PrintNumRec(int start, int end)
+{
+    if (start == end) return Convert.ToString(start);
+    else return (start + " " + PrintNumRec(start + 1, end));
+}
+
+int start = Prompt("Введите число M:");
+int end = Prompt("Введите число N:");
+
+
+if (start <= end)
+{
+    Console.WriteLine(PrintNumRec(start, end));
+}
+else
+{
+    Console.WriteLine(PrintNumRec(end, start));
+}
